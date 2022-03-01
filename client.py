b8e94cadeab8acb4453c46f5364dea1139a49557
Itamar Barashi <itamar.barashi1@gmail.com>
2022-03-01 15:15:26 +0200

    try

--- client.py
+++ client.py
@@ -2,12 +2,14 @@ import socket
 from settings import *
 
 def main():
-    load_balancer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_port , server_ip = join(my_socket)
 
 def join(server: socket):
     # Function that handles the client joining the server through the load balancer. Recives socket, returns port and ip of segment server.
     server.sendto(build_header.encode(), (IP, PORT))
-    port, addr = server.recvfrom((IP, PORT))
+    data , load_balancer_ip = server.recvfrom((IP, PORT))
+    data.decode()
     return 'hello  world'
 
 def build_header():
