a388ca376a6d88c405cdd7bffcf82fca2ab35bc2
Ido Minha <shufi.tolea@gmail.com>
2022-03-02 12:17:43 +0200

    Trunctated Player and Mob classes into a single Entity class

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from random import randint, randrange
+from random import randint, randrange, choice
 from os import path
 from animated_sprite import AnimatedSprite
 from settings import *
@@ -9,95 +9,18 @@ pg.init()
 WIDTH, HEIGHT = 1280, 720
 BLACK, RED = "#000000", "#FF0000"
 
-class Mob(pg.sprite.Sprite):
-    def __init__(self, pos, all_sprites):
-        self.groups = all_sprites
+class Entity(pg.sprite.Sprite):
+    def __init__(self, pos, sprite_groups, animations, walk_speed, anim_speed):
+        self.groups = sprite_groups
         pg.sprite.Sprite.__init__(self, self.groups)
 
-        self._start, self._end = (pos[0], pos[1]), (pos[0], pos[1])
-
-        self.status = 'idle'
-        self.health = randrange(20, 80)
-        self.animations = {
-            'idle': AnimatedSprite('graphics/small_dragon/Idle', 3, False),
-            'run': AnimatedSprite('graphics/small_dragon/Walk', 4, False),
-            'death': AnimatedSprite('graphics/Knight/KnightDeath_strip.png', 15, True)
-        }
-        self.animation = self.animations[self.status]
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.rect = self.image.get_rect(topleft=pos)
-        self.normal_image = self.image
-        self.image.fill((255, 0, 0))
-        
-        self.animation_tick = 0
-        self.animation_speed = 1  # TODO: do something with this
-
-        self.walk_speed = 2
-        self.direction = 0
-
-    def move(self, x, y):
-        self.change_status('run')
-        self._start = self.rect.center
-        self._end = self._start[0] + x, self._start[1] + y
-        dist = ((self._end[0] - self._start[0]) ** 2 + (self._end[1] - self._start[1]) ** 2) ** 0.5
-        self._t = dist / self.walk_speed
-        self._i = 0
-
-
-    def change_status(self, status):
-        self.status = status
-        self.animation = self.animations[self.status]
-
-
-    def update(self, map_rect):
-        if randint(1, 100) == 1:
-            dir_x = randint(-80, 80)
-            self.direction = dir_x < 0
-            self.move(dir_x, randint(-80, 80))
-
-        # RUNNING CALCULATIONS
-
-        if self.status == 'run':
-            if self._i < self._t:
-                self.rect.center = (min(max(round(self._start[0] + (self._end[0] - self._start[0]) * self._i / self._t), 0), map_rect.width),
-                                    min(max(round(self._start[1] + (self._end[1] - self._start[1]) * self._i / self._t), 0), map_rect.height))
-                self._i += 1
-
-            else:
-                self.rect.center = self._end
-                self.change_status('idle')
-
-        if self.animation_tick % 10 == 0:
-            self.animation.update()
-
-        self.animation_tick += 1
-        self.image = self.animation.image
-
-        if self.direction:
-            self.image = pg.transform.flip(self.image, True, False)
-
-    def draw(self, screen, camera):
-        screen.blit(self.image, camera.apply(self))
-        pg.draw.line(screen, (255, 0, 0),
-                     (camera.apply(self).topleft[0], camera.apply(self).topleft[1] - 20),
-                     (camera.apply(self).topleft[0] + self.health, camera.apply(self).topleft[1] - 20))
-
-
-class Player(pg.sprite.Sprite):
-    def __init__(self, pos, all_sprites):
-        self.groups = all_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
-
-        self.walk_speed = 5
+        self.walk_speed = walk_speed
+        self.anim_speed = anim_speed
         self._start, self._end = (pos[0], pos[1]), (pos[0], pos[1])
         self._i = 0
         self._t = 1
         self.status = 'idle'
-        self.animations = {
-            'idle': AnimatedSprite('graphics/Knight/KnightIdle_strip.png', 15, True),
-            'run': AnimatedSprite('graphics/Knight/KnightRun_strip.png', 8, True),
-            'death': AnimatedSprite('graphics/Knight/KnightDeath_strip.png', 15, True)
-        }
+        self.animations = animations
         self.animation = self.animations[self.status]
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.rect = self.image.get_rect(topleft=pos)
@@ -131,10 +54,7 @@ class Player(pg.sprite.Sprite):
             self.change_status('death')
             # TODO: fix death
             return
-        # for testing health:
-        if self.health < 100:
-            self.health += 0.1
-
+        
         # RUNNING CALCULATIONS
 
         if self.status == 'run':
@@ -147,7 +67,7 @@ class Player(pg.sprite.Sprite):
                 self.rect.center = self._end
                 self.change_status('idle')
 
-        if self.animation_tick % 4 == 0:
+        if self.animation_tick % self.anim_speed == 0:
             self.animation.update()
 
         self.animation_tick += 1
@@ -161,6 +81,11 @@ class Player(pg.sprite.Sprite):
         pg.draw.line(screen, (255, 0, 0),
                      (camera.apply(self).topleft[0], camera.apply(self).topleft[1] - 20),
                      (camera.apply(self).topleft[0] + self.health, camera.apply(self).topleft[1] - 20))
+        
+    
+
+def handle_keyboard(player, key):
+    pass
 
 
 def events(player, camera):
@@ -210,9 +135,22 @@ def run():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
 
     all_sprites = pg.sprite.Group()
+    
+    player_anims = {
+        'idle': AnimatedSprite('graphics/Knight/KnightIdle_strip.png', 15, True),
+        'run': AnimatedSprite('graphics/Knight/KnightRun_strip.png', 8, True),
+        'death': AnimatedSprite('graphics/Knight/KnightDeath_strip.png', 15, True)
+    }
+    mob_anims = [
+        {
+            'idle': AnimatedSprite('graphics/small_dragon/Idle', 3, False),
+            'run': AnimatedSprite('graphics/small_dragon/Walk', 4, False),
+            'death': AnimatedSprite('graphics/Knight/KnightDeath_strip.png', 15, True)
+        }
+    ]
 
-    player = Player((1400, 1360), all_sprites)
-    mob = Mob((1600, 1390), all_sprites)
+    player = Entity((1400, 1360), [all_sprites], player_anims, 5, 5)
+    mob = Entity((1600, 1390), [all_sprites], choice(mob_anims), 2, 15)
 
     # SETTING UP MAP
 
