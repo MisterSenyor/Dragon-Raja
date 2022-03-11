9e293e6bae768e583a3b7d0e057c37d890a66922
Itamar Barashi <itamar.barashi1@gmail.com>
2022-03-12 00:14:31 +0200

    my_server example

---
+++ my_server.py
@@ -0,0 +1,64 @@
+import json
+import time
+import threading
+import socket
+import settings
+import time
+
+
+class server:
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.socket.bind(settings.SERVER_ADDRESS)
+        print("Server ip: {}".format(self.ip))
+        self.clients = []
+        self.clientsAmount = 0
+        self.address = (self.ip, settings.SERVER_PORT)
+
+    def add(self, address):
+        self.clients.append(address)
+        data = json.dumps({'cmd': 'new', 'id': self.clientsAmount})
+        self.clientsAmount += 1
+        self.socket.sendto(data.encode(), address)
+
+    def send_data(self, data):
+        for i in self.clients:
+            self.socket.sendto(data, i)
+
+    def receive_packets(self, arr):
+        while True:
+            (msg, client_address) = self.socket.recvfrom(settings.HEADER_SIZE)
+            print("received packet. address: {}".format(client_address))
+            if client_address not in self.clients:
+                self.add(client_address)
+            try:
+                data = json.loads(msg.decode())
+                print("data: {}".format(data))
+                arr.append(data)
+            except Exception:
+                continue
+
+    def handle_updates(self, arr):
+        pass
+
+    def send_updates(self, arr):
+        data = json.dumps(arr).zfill(settings.HEADER_SIZE).encode()
+        self.send_data(data)
+        arr.clear()
+
+
+def main():
+    server_scoket = server()
+    arr = []
+    receive_thread = threading.Thread(target=server_scoket.receive_packets, args=(arr,))
+    handling_thread = threading.Thread(target=server_scoket.handle_updates, args=(arr,))
+    receive_thread.start()
+    handling_thread.start()
+    while True:
+        time.sleep(settings.UPDATE_TICK)
+        server_scoket.send_updates(arr)
+
+
+if __name__ == '__main__':
+    main()
