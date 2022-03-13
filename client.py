4df6d965888701e9cf6dd6679ca66a914f12aaf3
Ariel Ben Eliezer <dodibe2102@gmail.com>
2022-03-14 00:59:08 +0200

    removed old client code

--- client.py
+++ client.py
@@ -65,25 +65,3 @@ class Client:
                             self.handle_update(update)
                 except Exception:
                     logging.exception('exception while handling update')
-
-
-def main():
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address, data = join(my_socket)
-    print(data)
-
-
-def join(server: socket):
-    # Function that handles the client joining the server through the load balancer. Recives socket, returns server address ("IP",PORT) of server and data.
-    server.sendto(build_header().zfill(HEADER_SIZE).encode(), (IP, PORT))
-    data, server_address = server.recvfrom(HEADER_SIZE)
-    data = data.decode()
-    return server_address, data
-
-
-def build_header():
-    return 'hello world'
-
-
-if __name__ == '__main__':
-    main()
