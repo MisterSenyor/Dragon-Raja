11ee1964ad77e8e2c9f51f29aaf099fe47d0415b
Ariel Ben Eliezer <dodibe2102@gmail.com>
2022-03-14 00:58:22 +0200

    added items to client

--- client.py
+++ client.py
@@ -27,8 +27,10 @@ class Client:
 
         if cmd == 'new':
             entity = update['entity']
-            game.Entity(pos=entity['pos'], sprite_groups=self.all_sprite_groups, animations=self.player_animations,
-                        walk_speed=entity['walk_speed'], anim_speed=self.player_anim_speed, id_=entity['id'])
+            e = game.Entity(pos=entity['pos'], sprite_groups=self.all_sprite_groups, animations=self.player_animations,
+                            walk_speed=entity['walk_speed'], anim_speed=self.player_anim_speed, id_=entity['id'])
+            for item in entity['items']:
+                game.Item(item_type=item['type'], owner=e, id_=item['id'])
         else:
             ids = [entity.id for entity in entity_sprites.sprites()]
             entity = entity_sprites.sprites()[ids.index(update['id'])]
@@ -42,6 +44,10 @@ class Client:
                 projectile = update['projectile']
                 game.Projectile(proj_type=projectile['type'], attacker=entity, all_sprite_groups=self.all_sprite_groups,
                                 vect=pygame.Vector2(projectile['target']))
+            elif cmd == 'use_item':
+                item_ids = [item.id for item in entity.items.sprites()]
+                item = entity.items.sprites()[item_ids.index(update['item_id'])]
+                item.use_item()
             elif cmd == 'disconnect':
                 entity.kill()
 
