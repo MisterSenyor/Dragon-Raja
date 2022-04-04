516142bd106e50e4ca9ce1706552754ef2451f55
Omri <omri.reinharts@gmail.com>
2022-04-05 02:27:53 +0300

    added chat security

--- client_chat.py
+++ client_chat.py
@@ -1,40 +1,72 @@
 import socket
 import threading
 import settings
+from settings import *
 from entities import *
+from utils import *
 
 
-class chat_client:
+class ChatClient:
     socket: socket
 
     def __init__(self, name):
         self.name = name
+        self.seed = chat_start_seed
+        self.port = None
 
     def start(self):
+        # SET UP SOCKET
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            print("trying to connet with server: {}".format(settings.SERVER_ADDRESS_TCP))
-            self.socket.connect(settings.SERVER_ADDRESS_TCP)
-            msg = self.socket.recv(settings.HEADER_SIZE).decode()
+            # CONNECT TO SERVER:
+            print("trying to connet with server: {}".format(SERVER_ADDRESS_TCP))
+            self.socket.connect(SERVER_ADDRESS_TCP)
+
+            # GET PORT NUMBER:
+            self.port = (self.socket.getsockname()[1])
+
+            # RECEIVE MSG
+            msg = self.socket.recv(HEADER_SIZE).decode()
             print("the server sent: {}".format(msg))
-            self.socket.send(self.name.encode())
+
+            # ENCRYPT AND SEND NAME
+            encrypted_name = self.encrypt_data(self.name, True)
+            self.socket.send(encrypted_name)
         except Exception:
-            print("exception during start method")
+            logging.exception(f'Error while starting chat, closing connection')
 
     def receive(self, chat):
         while True:
             try:
-                message = self.socket.recv(settings.HEADER_SIZE).decode()
-                print("server sent: {}".format(message))
-                chat.add_line(message)
-            except:
-                print('Error receiving message from server, closing connection')
+                message = self.socket.recv(HEADER_SIZE).decode()
+                if message != '':
+                    print("server sent: {}".format(message))
+                    chat.add_line(message)
+            except Exception:
+                logging.exception(f'Error receiving message from server, closing connection')
 
     def send(self, data: str):
         try:
-            self.socket.send(data.encode())
+            # UPDATE SEED:
+            self.seed += (self.port + ascii_seed(chat_start_seed)) % 1000
+            encrypted = self.encrypt_data(data)
+            self.socket.send(encrypted)
         except Exception:
-            print("cant send data")
+            logging.exception(f'exception while sending data: {data}')
+
+    def encrypt_data(self, data: str, start=False):
+        """ encrypts and encodes data as bytes """
+
+        encrypted_data = data.encode()
+        if start:
+            self.seed = ascii_seed(self.seed) ^ self.port  # BARAK GONEN XOR'd WITH PORT
+        self.seed %= 150
+        # SHIFT LEFT BYTES:
+        encrypted_data = (int.from_bytes(encrypted_data, byteorder='big') << self.seed).to_bytes(
+            len(encrypted_data) + self.seed + self.seed % 3 + 1,
+            byteorder='big')
+        # RETURN ENCRYPTED DATA AS BYTES
+        return encrypted_data
 
     def handle(self):
         while True:
@@ -42,7 +74,7 @@ class chat_client:
 
 
 def main():
-    client1 = chat_client("Itamar")
+    client1 = ChatClient("Itamar")
     client1.start()
     receive_thread = threading.Thread(target=client1.handle)
     receive_thread.start()
--- server_chat.py
+++ server_chat.py
@@ -1,9 +1,27 @@
 import socket
 import threading
-import settings
+from settings import *
+from utils import *
 
 
-class chat_server:
+def decrypt_data(data: bin, port, seed, start=False):
+    """ decrypts data via port and seed, returns string"""
+
+    # GET CORRECT SEED FOR FIRST TIME DATA ENCRYPTED:
+    if start:
+        seed = ascii_seed(chat_start_seed) ^ port  # BARAK GONEN XOR'd WITH PORT
+    seed %= 150
+    # SHIFT RIGHT BYTES:
+    data = (int.from_bytes(data, byteorder='big') >> seed).to_bytes(
+        len(data) - seed - seed % 3 - 1,
+        byteorder='big')
+
+    # RETURN DATA AS STRING
+    data = data.decode()
+    return data
+
+
+class ChatServer:
     socket: socket
 
     def __init__(self):
@@ -12,32 +30,44 @@ class chat_server:
 
     def start(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(settings.SERVER_ADDRESS_TCP)
+        self.socket.bind(SERVER_ADDRESS_TCP)
         self.socket.listen()
         print("Chat server is up and running")
         while True:
             client, address = self.socket.accept()
             print(f'connection is established with {str(address)}')
             client.send("Connected successfully".encode())
-            name = client.recv(settings.HEADER_SIZE).decode()
+            name = client.recv(HEADER_SIZE)
+
+            # DECRYPT WITH START SEED
+            client_port = client.getpeername()[1]
+            seed = ascii_seed(chat_start_seed) ^ client_port  # BARAK GONEN XOR'd WITH PORT
+            name = decrypt_data(name, client_port, seed, start=True)
+
             self.clients.append(client)
             self.names.append(name)
             print('The name of this client is {}'.format(name))
-            client_thread = threading.Thread(target=self.handle_client, args=(client,))
+            client_thread = threading.Thread(target=self.handle_client, args=(client, seed,))
             client_thread.start()
 
-    def handle_client(self, client: socket):
+    def handle_client(self, client: socket, seed):
+        # GET CLIENT PORT:
+        client_port = client.getpeername()[1]
         while True:
             index = self.clients.index(client)
             name = self.names[index]
             try:
-                message = client.recv(settings.HEADER_SIZE).decode()
-                print("{} sent {}".format(name, message))
-                if (message != ""):
-                    self.broadcast("{}: {}".format(name, message))
+                message = client.recv(HEADER_SIZE)
+                if message != "":
+                    # CHANGE SEED:
+                    seed += (client_port + ascii_seed(chat_start_seed) % 1000)
+                    data = decrypt_data(message, client_port, seed)
+                    print("{} sent {}".format(name, data))
+                    self.broadcast("{}: {}".format(name, data))
                 else:
-                    raise Exception
+                    logging.error(f'Client sent empty string via chat: {name, client}')
             except Exception:
+                logging.exception(f'exception while handling chat client: {name, client}')
                 self.clients.remove(client)
                 self.names.remove(name)
                 print(f"{name} has disconnected from server")
@@ -53,7 +83,7 @@ class chat_server:
 
 
 def main():
-    server1 = chat_server()
+    server1 = ChatServer()
     server1.start()
 
 
