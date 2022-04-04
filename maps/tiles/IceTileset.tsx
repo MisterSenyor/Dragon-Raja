b73184465c1495fac19c5e6ecec59aca2ec174ba
NoNo-13 <noamshavit111@gmail.com>
2022-04-04 18:08:59 +0300

    images for collisions

--- maps/tiles/IceTileset.tsx
+++ maps/tiles/IceTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.1" name="IceTileset" tilewidth="64" tileheight="64" tilecount="40" columns="10">
- <image source="../../../../Desktop/Tiles/IceTileset.png" width="640" height="288"/>
+ <image source="../images/IceTileset.png" width="640" height="288"/>
 </tileset>
--- maps/tiles/tree.tsx
+++ maps/tiles/tree.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.1" name="tree" tilewidth="64" tileheight="64" tilecount="4" columns="2">
- <image source="../../Graphics/tree.png" width="128" height="128"/>
+ <image source="../images/tree.png" width="128" height="128"/>
 </tileset>
